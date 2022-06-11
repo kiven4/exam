81ee6615a89fa971cfaa5a2d7b6704b19d7ef344
kiven4 <107303525+kiven4@users.noreply.github.com>
2022-06-11 18:39:48 +0100

    Add files via upload

---
+++ problem3.py
@@ -0,0 +1,7 @@
+def  two_sum(nums,target):
+    nums=(2,7,11,15)
+    target=9
+    nums[0:3]==9
+    print(target)
+        
+
---
+++ problem4.py
@@ -0,0 +1,5 @@
+def size(sum):
+  sum=1,4,2,5
+  for x in range(list(sum)):
+      sum=+1
+      print(sum)
